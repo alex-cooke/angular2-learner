3fd62a0cf1e41c8d42937a220cf802290d5a84dc
Alexander Cooke <alex.cooke@live.com>
2016-12-10 20:40:55 +1000

    add ngFor and ngIf examples into app.component

--- source/app/app.component.ts
+++ source/app/app.component.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{ message }}</h1>'
+  template: `
+<h1>{{ message }}</h1>
+<hr>
+<div>
+  <div *ngFor="let item of items">{{ item }}</div>
+</div>
+<div *ngIf="items.length">You have {{ items.length }} items</div>
+`
 })
 export class AppComponent {
+
   message: string;
+  items: any[];
+
   constructor() {
+
     this.message = "Hello, World!";
     console.log(this.message);
+
+    this.items = [
+      'item 1',
+      'item 2',
+      'item 3'
+    ];
+
+
   }
 }
